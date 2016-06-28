24d8fd5bbd6f699fa0afa4e8ad02bab1fafa6dbe
AxelNorth <trajanka.milkova@gmail.com>
2016-06-28 15:54:57 +0200

    Update MainActivity.java

--- test/GreenhouseCI/src/main/java/com/greenhouseci/MainActivity.java
+++ test/GreenhouseCI/src/main/java/com/greenhouseci/MainActivity.java
@@ -4,6 +4,52 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import net.hockeyapp.android.CrashManager;
+
+public class YourActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Your own code to create the view
+    // ...
+
+    checkForUpdates();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    // ... your own onResume implementation
+    checkForCrashes();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    unregisterManagers();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    unregisterManagers();
+  }
+
+  private void checkForCrashes() {
+    CrashManager.register(this);
+  }
+
+  private void checkForUpdates() {
+    // Remove this for store builds!
+    UpdateManager.register(this);
+  }
+
+  private void unregisterManagers() {
+    UpdateManager.unregister();
+  }
+
+}
 public class MainActivity extends Activity {
 
     @Override
